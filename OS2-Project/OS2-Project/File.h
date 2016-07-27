804598c65470833867d391383687a0a7c7c0526a
Ilija Knezevic <ilija.knezevic94@gmail.com>
2016-07-27 18:00:31 +0200

    part.lib

--- OS2-Project/OS2-Project/File.h
+++ OS2-Project/OS2-Project/File.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "FS.h"
+
+class KernelFile;
+class File {
+public:
+	~File(); //zatvaranje fajla
+	char write(BytesCnt, char* buffer);
+	BytesCnt read(BytesCnt, char* buffer);
+	char seek(BytesCnt);
+	BytesCnt filePos();
+	char eof();
+	BytesCnt getFileSize();
+	char truncate();
+private:
+	friend class FS;
+	friend class KernelFS;
+	File(); //objekat fajla se može kreirati samo otvaranjem
+	KernelFile *myImpl;
+};
